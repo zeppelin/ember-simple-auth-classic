3b9c3c4ec5202b4937ebe0906042d7c17a4aa378
Gabor Babicz <gabor.babicz@gmail.com>
2014-09-24 12:48:44 +0200

    Use wildcard domain when clearing cookie

--- dist/globals/simple-auth-classic.js
+++ dist/globals/simple-auth-classic.js
@@ -169,7 +169,7 @@ exports["default"] = Base.extend({
     var path = '; path=/';
     var expires = Ember.isEmpty(expiration) ? '' : '; expires=' + new Date(expiration).toUTCString();
     var secure  = !!this._secureCookies ? ';secure' : '';
-    var domain  = '; domain=' + document.domain; // TODO do i need domain?
+    var domain  = '; domain=' + this.extractWildcardDomain();
     document.cookie = this.cookieName + '=' + encodeURIComponent(value) + path + expires + secure + domain;
   },
 
@@ -183,7 +183,24 @@ exports["default"] = Base.extend({
       Ember.run.cancel(this._syncDataTimeout);
       this._syncDataTimeout = Ember.run.later(this, this.syncData, 500);
     }
-  }
+  },
+
+  extractWildcardDomain: function() {
+    var ipv6Regex = /^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i;
+    var ipv4Regex = /^(\d{1,3}\.){3}(\d{1,3})$|^(0x[\da-fA-F]{2}\.){3}(0x[\da-fA-F]{2})$|^(0[0-3][0-7]{2}\.){3}(0[0-3][0-7]{2})|^0x[\da-fA-F]{8}$|^[0-4]\d{9}$|^0[0-3]\d{10}$/;
+    var documentDomain = document.domain;
+    var domain;
+
+    if (documentDomain === 'localhost') {
+      return '';
+    } else if (domain = documentDomain.match(ipv6Regex)) {
+      return domain[0];
+    } else if (domain = documentDomain.match(ipv4Regex)) {
+      return domain[0];
+    }
+
+    return '.' + documentDomain.split('.').slice(-2).join('.');
+  },
 });
 },{}]},{},[4])
 (4)
